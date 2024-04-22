b45e70fa3b6f774a05743035223764db76b5f263
catjofupit3r <catofjupit3r@gmail.com>
2024-04-22 10:41:55 +0300

    feat: add additional data parameter for Exception class to provide extra data for requests.

--- src/middleware/ErrorHandlerMiddleware.ts
+++ src/middleware/ErrorHandlerMiddleware.ts
@@ -3,7 +3,7 @@ import { Exception } from '../models/ErrorModels'
 
 export const errorHandlerMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Exception) {
-        res.status(err.status).json({ message: err.message })
+        res.status(err.status).json({ message: err.message, ...err.additionalData })
     } else {
         console.log('Caught undocumented error:', err)
         res.status(500).json({ message: 'Internal server error' })
--- src/models/ErrorModels.ts
+++ src/models/ErrorModels.ts
@@ -1,39 +1,47 @@
+interface AdditionalData {
+    [key: string]: any
+}
+
 export class Exception extends Error {
     constructor(
         public message: string,
-        public status: number
+        public status: number,
+        public additionalData: AdditionalData = {}
     ) {
         super(message)
         this.status = status
+        if (additionalData) {
+            this.additionalData = additionalData
+        }
     }
 }
 
 export class NotFound extends Exception {
-    constructor(message: string = 'Data you are looking for was not found') {
-        super(message, 404)
+    constructor(message: string = 'Data you are looking for was not found', additionalData?: AdditionalData) {
+        super(message, 404, additionalData)
     }
 }
 
 export class Unauthorized extends Exception {
-    constructor(message: string = 'Your request is not authorized') {
-        super(message, 401)
+    constructor(message: string = 'Your request is not authorized', additionalData?: AdditionalData) {
+        super(message, 401, additionalData)
     }
 }
 
 export class BadRequest extends Exception {
-    constructor(message: string = 'Your request is malformed') {
-        super(message, 400)
+    constructor(message: string = 'Your request is malformed', additionalData?: AdditionalData) {
+        super(message, 400, additionalData)
     }
 }
 
 export class InternalServerError extends Exception {
-    constructor(message: string = 'Something went wrong on backend side') {
-        super(message, 500)
+    constructor(message: string = 'Something went wrong on backend side', additionalData?: AdditionalData) {
+        super(message, 500, additionalData)
     }
 }
 
 export class Forbidden extends Exception {
-    constructor(message: string = 'You are not allowed to access this resource') {
-        super(message, 403)
+    constructor(message: string = 'You are not allowed to access this resource', additionalData?: AdditionalData) {
+        super(message, 403, additionalData)
     }
 }
