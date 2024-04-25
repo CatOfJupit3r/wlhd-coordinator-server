66c13ca7e7e2709d450521e7361f581e4a418368
CatOfJupit3r <catofjupit3r@gmail.com>
2024-04-25 23:26:56 +0300

    feat: change default port to 5000.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import app from './app'
 dotenv.config()
 
-const PORT = (process.env.PORT && parseInt(process.env.PORT)) || 4000
+const PORT = (process.env.PORT && parseInt(process.env.PORT)) || 5000
 const HOST = process.env.HOST || 'localhost'
 
 app.listen(PORT, HOST, () => {
