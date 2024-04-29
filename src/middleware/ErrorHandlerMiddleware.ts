db8150be5a88016614e917111e98fa663d6a4525
CatOfJupit3r <catofjupit3r@gmail.com>
2024-04-29 18:52:06 +0300

    feat[error handling]: handle error case for bad JSONs.

--- src/middleware/ErrorHandlerMiddleware.ts
+++ src/middleware/ErrorHandlerMiddleware.ts
@@ -7,6 +7,11 @@ export const errorHandlerMiddleware = (err: Error, req: Request, res: Response, 
         res.status(err.status).json({ message: err.message, ...err.additionalData })
         if (err instanceof CriticalError) process.exit(1)
     } else {
+        // If clients sends bad JSON, express json lib will throw SyntaxError
+        // We can catch it and send a proper response
+        if (err instanceof SyntaxError && 'status' in err && err.status === 400 && 'body' in err) {
+            return res.status(400).send({ status: 400, message: err.message })
+        }
         console.log('Caught undocumented error:', err)
         res.status(500).json({ message: 'Internal server error' })
         next(err)
