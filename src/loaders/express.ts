ddf81d03e0b48b964a73a72b8948abe6ab3b6849
CatOfJupit3r <catofjupit3r@gmail.com>
2024-04-30 23:54:18 +0300

    refactor: move imports.

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -2,13 +2,14 @@ import cors from 'cors'
 import express, { Express } from 'express'
 import 'express-async-errors'
 
-import { errorHandlerMiddleware } from '../middleware/ErrorHandlerMiddleware'
 import combatRoutes from '../routes/combatRoutes'
 import entityRoutes from '../routes/entityRoutes'
 import indexRoutes from '../routes/intexRoutes'
 import lobbyRoutes from '../routes/lobbyRoutes'
 import translationRoutes from '../routes/translationRoutes'
 import userRoutes from '../routes/userRoutes'
+
+import { errorHandlerMiddleware } from '../middleware/ErrorHandlerMiddleware'
 
 const ExpressLoader = async (app: Express) => {
     app.use(cors())
@@ -21,7 +22,7 @@ const ExpressLoader = async (app: Express) => {
     app.use('/combat', combatRoutes)
     app.use('/entity', entityRoutes)
 
-    app.use(errorHandlerMiddleware) // check if middleware is applied on login endpoints.
+    app.use(errorHandlerMiddleware)
 }
 
 export default ExpressLoader
