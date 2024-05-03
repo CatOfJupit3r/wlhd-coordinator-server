b9b72965ae8b05c12ed88102138df80a01049d58
CatOfJupit3r <catofjupit3r@gmail.com>
2024-05-03 23:59:30 +0300

    chore: remove debug console.log() statement

--- src/middleware/ErrorHandlerMiddleware.ts
+++ src/middleware/ErrorHandlerMiddleware.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from 'express'
 import { CriticalError, Exception } from '../models/ErrorModels'
 
 export const errorHandlerMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log(err instanceof Exception, err instanceof CriticalError)
     if (err instanceof Exception) {
         res.status(err.status).json({ message: err.message, ...err.additionalData })
         if (err instanceof CriticalError) process.exit(1)
