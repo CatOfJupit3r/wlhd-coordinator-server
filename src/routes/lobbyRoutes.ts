34cbd7dfddcc07765e3c39a2a30268bde2fb2940
CatOfJupit3r <catofjupit3r@gmail.com>
2024-05-31 18:26:22 +0300

    refactor: move authmiddleware application to be global for route.

--- src/routes/lobbyRoutes.ts
+++ src/routes/lobbyRoutes.ts
@@ -4,36 +4,22 @@ import { authenticationMiddleware } from '../middleware/AuthenticationMiddleware
 
 const router = Router()
 
+router.use(authenticationMiddleware)
+
 // GET
 
-router.get(
-    '/:lobby_id/my_character',
-    authenticationMiddleware,
-    LobbyController.getMyCharacterInfo.bind(LobbyController)
-)
-router.get(
-    '/:lobby_id/character/:character_id',
-    authenticationMiddleware,
-    LobbyController.getCharacterInfo.bind(LobbyController)
-)
-router.get('/:lobby_id', authenticationMiddleware, LobbyController.getLobbyInfo.bind(LobbyController))
+router.get('/:lobby_id/my_character', LobbyController.getMyCharacterInfo.bind(LobbyController))
+router.get('/:lobby_id/character/:character_id', LobbyController.getCharacterInfo.bind(LobbyController))
+router.get('/:lobby_id', LobbyController.getLobbyInfo.bind(LobbyController))
 
 // POST
 
-router.post('/create', authenticationMiddleware, LobbyController.createNewLobby.bind(LobbyController))
-router.post(
-    '/:lobby_id/create_combat',
-    authenticationMiddleware,
-    LobbyController.createCombatForLobby.bind(LobbyController)
-)
+router.post('/create', LobbyController.createNewLobby.bind(LobbyController))
+router.post('/:lobby_id/create_combat', LobbyController.createCombatForLobby.bind(LobbyController))
 
 // PATCH
 
-router.patch('/:lobby_id/add_player', authenticationMiddleware, LobbyController.addPlayerToLobby.bind(LobbyController))
-router.patch(
-    '/:lobby_id/assign_character',
-    authenticationMiddleware,
-    LobbyController.assignCharacterToPlayer.bind(LobbyController)
-)
+router.patch('/:lobby_id/add_player', LobbyController.addPlayerToLobby.bind(LobbyController))
+router.patch('/:lobby_id/assign_character', LobbyController.assignCharacterToPlayer.bind(LobbyController))
 
 export default router
